87c228d1fb3957ac987062a3df890175e56bddcf
Nathan Gardiner <ngardiner@gmail.com>
2025-06-23 22:54:14 +1000

    improve layout button style

--- web/src/components/EditButton.jsx
+++ web/src/components/EditButton.jsx
@@ -7,31 +7,32 @@ const EditButton = ({ isEditing, onClick }) => {
       onClick={onClick}
       title={isEditing ? "Exit edit mode" : "Edit dashboard layout"}
     >
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="18" 
-        height="18" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round"
-      >
-        {isEditing ? (
-          // X icon for cancel
-          <>
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </>
-        ) : (
-          // Pencil icon for edit
-          <>
-            <path d="M12 20h9"></path>
-            <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-          </>
-        )}
-      </svg>
+      {isEditing ? (
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="18" 
+          height="18" 
+          viewBox="0 0 18 18"
+        >
+          <path 
+            d="M 4 4 L 14 14 M 14 4 L 4 14"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
+      ) : (
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="18" 
+          height="18" 
+          viewBox="0 0 18 18" 
+          fill="currentColor"
+        >
+          <path d="M 2 2 h 6 v 6 h -6 z M 10 2 h 6 v 6 h -6 z M 2 10 h 6 v 6 h -6 z M 10 10 h 6 v 6 h -6 z" />
+        </svg>
+      )}
     </button>
   );
 };
